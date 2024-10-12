5d2b2d45862eb75b4df5e7f6e73612c58cf12b94
Cassio Santana <cassioo.roger@gmail.com>
2024-10-12 15:11:13 -0300

    String

--- js-fundamentos/numerosAlgunsCuidados.js
+++ js-fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,7 @@
 console.log(7 / 0) // Infinity. Javascript possui esse tipo.
 console.log("10" / 2) // Como a string é de exclusivamente um valor numérico a linguagem entende que é a operação matemática que desejamos realizar.
+console.log('3' + 2)
+console.log('3' - 2)
 console.log("Show!" / 2)
 console.log(0.1 + 0.7) // 0.7999999999999999
 // console.log(10.toString()) // SyntaxError: Invalid or unexpected token
