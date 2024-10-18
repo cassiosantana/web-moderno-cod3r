6da108d1307f9eb7d784414b10f5a49952a43034
Cassio Santana <cassioo.roger@gmail.com>
2024-10-18 10:18:41 -0300

    Quase tudo é função

---
+++ js-fundamentos/funcaoEmQuaseTudo.js
@@ -0,0 +1,12 @@
+console.log(typeof Object)
+
+class Produto {}
+console.log(typeof Produto)
+
+/**
+ * Qualquer classe me JS será uma função. "class" é apenas um artifício da linguagem para nos guiar.
+ * A função pode ter atributos.
+ * Uma função pode ter outras funções.
+ * É possível armazenar uma função em uma variável.
+ * É possível passar uma função como parâmetro.
+ */
