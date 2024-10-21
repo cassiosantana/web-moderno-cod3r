2efbe99d502aedc41618787917760d77f9b69c4c
Cassio Santana <cassioo.roger@gmail.com>
2024-10-21 09:31:17 -0300

    Declaração de variáveis com let

---
+++ js-fundamentos/usandoLet1.js
@@ -0,0 +1,18 @@
+// Exemplo 1
+console.log('### Exemplo 1 ###')
+var numero = 1
+{
+    let numero = 2
+    console.log('Dentro', numero)
+}
+console.log('Fora', numero)
+
+
+// Exemplo 2
+console.log('\n### Exemplo 2 ###')
+let numero2 = 3
+{
+    let numero2 = 4
+    console.log('Dentro', numero2)
+}
+console.log('Fora', numero2)
